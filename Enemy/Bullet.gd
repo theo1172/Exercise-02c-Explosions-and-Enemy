8408e5fdd6e64f22e7787f57c2a72c95e92a48ae
theo1172 <theoahawkins@gmail.com>
2023-02-02 14:50:03 -0500

    completes exercise

--- Enemy/Bullet.gd
+++ Enemy/Bullet.gd
@@ -15,14 +15,15 @@ func _physics_process(_delta):
 	position.y = wrapf(position.y, 0, 600)
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("damage"):
-		body.damage(damage)
-	Effects = get_node_or_null("/root/Game/Effects")
-	if Effects != null:
-		var explosion = Explosion.instance()
-		Effects.add_child(explosion)
-		explosion.global_position = global_position 
-	queue_free()
+	if body.name != "Enemy":
+		if body.has_method("damage"):
+			body.damage(damage)
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position 
+		queue_free()
 
 func _on_Timer_timeout():
 	queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,9 @@ var velocity = Vector2.ZERO
 var rotation_speed = 5.0
 var speed = 5.0
 var max_speed = 400.0
-
+var health = 10
+var Effects = null
+onready var Explosion = load("res://Effects/Explosion.tscn")
 
 onready var Bullet = load("res://Player/Bullet.tscn")
 var nose = Vector2(0,-60)
@@ -21,7 +23,7 @@ func _physics_process(_delta):
 	position.y = wrapf(position.y, 0, 600)
 
 	if Input.is_action_just_pressed("shoot"):
-		var Effects = get_node_or_null("/root/Game/Effects")
+		Effects = get_node_or_null("/root/Game/Effects")
 		if Effects != null:
 			var bullet = Bullet.instance()
 			bullet.global_position = global_position + nose.rotated(rotation)
@@ -42,4 +44,18 @@ func get_input():
 		rotation_degrees = rotation_degrees + rotation_speed
 	return to_return.rotated(rotation)
 	
-		
+func damage(d):
+	health -= d
+	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			yield(explosion, "animation_finished")
+		queue_free()
+
+func _on_Area2D_body_entered(body):
+	if body.name != "Player":
+		damage(100)
